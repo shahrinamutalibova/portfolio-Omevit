f019a8fa3617399e17b0fa1032802b53f49d2f4e
shahrinamutalibova <shahrinamutalibova@gmail.com>
2023-08-29 17:23:12 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+AOS.init();
+const productToModal = () => {
+    const Modal = [];
+    let index = 0;
+}
+const toggleModal = () => {
+    document.getElementById("product_modal").classList.toggle("active_Modal");
+    productToModal();
+}
+const exampleModal = document.getElementById('exampleModal')
+if (exampleModal) {
+  exampleModal.addEventListener('show.bs.modal', event => {
+    // Button that triggered the modal
+    const button = event.relatedTarget
+    // Extract info from data-bs-* attributes
+    const recipient = button.getAttribute('data-bs-whatever')
+    // If necessary, you could initiate an Ajax request here
+    // and then do the updating in a callback.
+
+    // Update the modal's content.
+    const modalTitle = exampleModal.querySelector('.modal-title')
+    const modalBodyInput = exampleModal.querySelector('.modal-body input')
+
+    modalTitle.textContent = `New message to ${recipient}`
+    modalBodyInput.value = recipient
+  })
+}
+productToModal();
